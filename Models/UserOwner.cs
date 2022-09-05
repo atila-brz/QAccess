9d0ee0815162d8ce420cd5668c64660930e35633
Gildson Carvalho <gildson.carvalho@gmail.com>
2022-09-05 16:58:39 -0300

    feat:Create files UserOwner.cs and UserEmployee.cs

---
+++ Models/UserOwner.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QAccess.Models
+{
+    public class UserOwner
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string MaritalStatus { get; set; }
+        public string ContactNumber { get; set; }
+        private string cpf;
+        public string Cpf
+        {
+
+        }
+        
+        // precisa rever se vai ter a possibilidade de ter varia unidade por pessoa pq ai vai ter de usar uma lista de unidades
+        public Unit Unit { get; set; }
+
+        private bool ValidateCpf(string cpf)
+        {
+            
+        }
+
+    }
+}

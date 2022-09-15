4abec6a32bec8d1d568b55aad8baea61461e7a34
Atila Bezerra <bezerraatila2@gmail.com>
2022-09-15 14:30:22 -0300

    script install requeriments

---
+++ requeriments.bat
@@ -0,0 +1,9 @@
+dotnet tool install --global dotnet-ef
+dotnet tool install --global dotnet-aspnet-codegenerator
+
+dotnet add package Pomelo.EntityFrameworkCore.Mysql 
+dotnet add package Pomelo.EntityFrameworkCore.MySql.Design
+dotnet add package Microsoft.EntityFrameworkCore.Tools
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+dotnet add package Microsoft.VisualStudio.Web.CodeGenerators.Mvc
+dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design

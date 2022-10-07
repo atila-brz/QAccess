1569dea685fdf91767cb8a662146ff3d86ee8165
Gildson Carvalho <gildson.carvalho@gmail.com>
2022-10-07 15:22:34 -0300

    changes in models

--- Models/Condominium.cs
+++ Models/Condominium.cs
@@ -2,17 +2,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace QAccess.Models
 {
     public class Condominium
     {
+        [Required]
+        [Key]
         public string CondominiumId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Nome")]
         public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Gênero")]
         public string Gender { get; set; }
+
+        [Required]
+        [Display(Name = "Data de Nascimento")]
         public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Estado Civil")]
         public string MaritalStatus { get; set; }
+
+        [Required]
+        [StringLength(11)]
+        [Display(Name = "Contato")]
         public string ContactNumber { get; set; }
+
+        [Required]
+        [StringLength(11)]
         public string Cpf { get; set; }
         public virtual ICollection<Unit>? Units { get; set; }
 

bf58cfb983d6623c13003da05077fb7a5c3f0bf3
Atila <atila.bzalm@gmail.com>
2022-11-15 23:55:12 -0300

    feat: validate modal actions

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const unitsModal = document.getElementById('unitsModal');
+
+unitsModal.addEventListener('show.bs.modal', event => {
+
+    const awareUnits = unitsModal.querySelector('#awareUnits');
+    const buttonUnitID = unitsModal.querySelector('#unitButtonId');
+
+    buttonUnitID.disabled = true;
+    awareUnits.checked = false;
+
+    awareUnits.addEventListener( 'change', function() {
+
+        const button = event.relatedTarget
+        const recipient = button.getAttribute('data-bs-whatever')
+        
+        if(this.checked) {
+            buttonUnitID.disabled = false;
+            const inputDeleteUnitID = unitsModal.querySelector('#unitInputID')
+            inputDeleteUnitID.value = recipient
+
+        } else {
+            buttonUnitID.disabled = true;
+        }
+    });
+})
